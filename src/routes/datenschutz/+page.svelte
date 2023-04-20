516d25d08d6264a91d53c7f327ae3d326dc01f32
Dietrich Poensgen <poedietrich@outlook.de>
2023-04-20 20:36:33 +0200

    datenschutzerklärung

---
+++ src/routes/datenschutz/+page.svelte
@@ -0,0 +1,58 @@
+<main class="prose xl:prose-lg p-10 m-auto">
+    <h1>Datenschutz</h1>
+    <p>Angaben gemäß § 5 TMG</p>
+    <p>
+        Herzog-Ernst-Gymnasium Uelzen<br />
+        z. Hd. Dietrich Poensgen<br />
+        Albertstraße 41<br />
+        29525 Uelzen
+    </p>
+    <p>
+        <strong>Kontakt:</strong>
+        <br />
+        E-Mail: hegame.app[at]heg-portal.de
+    </p>
+
+    <p>
+        Die Nutzung der App ist in der Regel ohne Angabe personenbezogener Daten
+        möglich. Soweit auf unserer App personenbezogene Daten (beispielsweise
+        Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies,
+        soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne
+        Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.<br />
+        Wir weisen darauf hin, dass die generelle Datenübertragung im Internet (z.B.
+        bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser
+        Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.<br />
+        Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten
+        durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung
+        und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die
+        Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle
+        der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails,
+        vor.<br />
+    </p>
+
+    <h2>Zugriff auf Funktionen des Endgeräts</h2>
+    <p>
+        Für die Benutzung der App ist folgender Schnittstellenzugriff des
+        Endgeräts notwendig:
+    </p>
+
+    <h3>Kamerazugriff</h3>
+    <p>
+        Der Kamerazugriff wird benötigt, um die Kernkomponente des Spiels, die
+        QR-Codes, zu erfassen und verarbeiten zu können.
+    </p>
+    <p>
+        Die bei dem Kamerazugriff erfassten Daten werden nur lokal auf dem Gerät
+        verarbeitet, es erfolgt dabei keine Übertragung der Daten an Dritten
+        durch die App.
+    </p>
+    <h2>Änderung unserer Datenschutzbestimmungen</h2>
+    <p>
+        Wir behalten uns vor, diese Datenschutzerklärung gelegentlich
+        anzupassen, damit sie stets den aktuellen rechtlichen Anforderungen
+        entspricht oder um Änderungen unserer Leistungen in der
+        Datenschutzerklärung umzusetzen, z. B. bei der Einführung neuer
+        Services. Für Ihre weitere Nutzung dieser App gilt dann die neue
+        Datenschutzerklärung.
+    </p>
+</main>
